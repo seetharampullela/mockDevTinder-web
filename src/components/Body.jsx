0320ac0e4d86f84d25df879252592c782f1a44ba
seetharampullela <seetharampullela@gmail.com>
2026-04-23 13:34:16 +0530

    changes for secured routing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigate();
+  const userData = useSelector((state) => state.user);
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log("res>>>> ", res);
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status == 401) {
+        navigation("/login");
+      }
+      console.log("ERROR: ", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Navbar />
