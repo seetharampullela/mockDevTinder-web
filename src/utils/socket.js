05ae314324c3720ccad03a3f76d131f3c41c8fec
seetharampullela <seetharampullela@gmail.com>
2026-04-23 13:41:50 +0530

    socket issue

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,10 +2,9 @@ import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  return io(BASE_URL, {
-    auth: async (cb) => {
-      const tokenObj = await cookieStore.get("token");
-      cb({ token: tokenObj?.value });
-    },
-  });
+  if (location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
 };
